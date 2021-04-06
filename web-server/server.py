bde23ae45d7051f15388fc64404dc46b9963816a
Ayoub.Tammaoui <ayoub.tammaoui@student.uib.no>
2021-04-06 15:47:16 +0200

    Added button reload

--- web-server/server.py
+++ web-server/server.py
@@ -1,29 +1,33 @@
 import os
 
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
-
-from util import dummy_data
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Retrieves the variables necessary to assemble the MONGO URI
-MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME')
-MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
-MONGODB_HOSTNAME = os.environ.get('MONGODB_HOSTNAME')
-MONGODB_DATABASE = os.environ.get('MONGODB_DATABASE')
+DEBUG = os.environ.get('DEBUG', False)
 
-app.config["MONGO_URI"] = f'mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}' \
+
+if DEBUG:
+    # Retrieves the variables necessary to assemble the MONGO URI
+    MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME')
+    MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
+    MONGODB_HOSTNAME = os.environ.get('MONGODB_HOSTNAME')
+    MONGODB_DATABASE = os.environ.get('MONGODB_DATABASE')
+    URI = f'mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}' \
     '@{MONGODB_HOSTNAME}:27017/{MONGODB_DATABASE}'
+    client = MongoClient(uri=URI)
+else:
+    URI = 'mongodb://root:example@localhost:27017/'
+    client = MongoClient(URI)
 
-# Instantiates the connection to the database.
-mongo = PyMongo(app)
-db = mongo.db
+db = client.weather
 
 
 @app.route('/')
 def home_page():
-    return render_template('index.html', weather_data=dummy_data)
+    weather_data = list(db.weather.find({}))
+    return render_template('index.html', weather_data=weather_data)
 
 
 if __name__ == '__main__':
