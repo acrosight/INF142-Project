2711f4f719ea13a9e245272354627c5d9e23d258
Ayoub.Tammaoui <ayoub.tammaoui@student.uib.no>
2021-04-06 16:13:34 +0200

    Fixed bug

--- web-server/server.py
+++ web-server/server.py
@@ -5,10 +5,13 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 
 if DEBUG:
+    URI = 'mongodb://root:example@localhost:27017/'
+    client = MongoClient(URI)
+else:
     # Retrieves the variables necessary to assemble the MONGO URI
     MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME')
     MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
@@ -16,19 +19,14 @@ if DEBUG:
     MONGODB_DATABASE = os.environ.get('MONGODB_DATABASE')
     URI = f'mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}' \
     '@{MONGODB_HOSTNAME}:27017/{MONGODB_DATABASE}'
-    client = MongoClient(uri=URI)
-else:
-    URI = 'mongodb://root:example@localhost:27017/'
     client = MongoClient(URI)
 
-db = client.weather
-
+db = client.weatherstation
 
 @app.route('/')
 def home_page():
     weather_data = list(db.weather.find({}))
     return render_template('index.html', weather_data=weather_data)
-
 
 if __name__ == '__main__':
     app.run()
